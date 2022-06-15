8735f377d05c2137f0af38d6fcaf351cf25be802
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-06-15 16:19:39 -0500

    IConexión con la API

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
-
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
-
-intersectionObserver.observe($observe);
+const getData = async () => {
+	try {
+		let res = await fetch(API);
+		const data = await res.json();
+		printCard(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
