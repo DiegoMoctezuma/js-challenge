cfaccc4ea91be15f88265c37f14a88b0ddcb0df3
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-06-15 16:28:40 -0500

    Pintando las Cards con data de API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const $app = document.getElementById("app");
 const API = "https://api.escuelajs.co/api/v1/products";
+let minId = 5;
+let maxId = 15;
 
 const getData = async () => {
 	try {
@@ -9,4 +11,23 @@ const getData = async () => {
 	} catch (error) {
 		console.log(error);
 	}
+};
+
+const printCard = (data) => {
+	const filter = data.filter((item) => item.id >= minId && item.id < maxId);
+	const cards = document.querySelector("#items");
+	const templateCard = document.querySelector("#templateCard");
+	const fragment = document.createDocumentFragment();
+
+	filter.forEach((item) => {
+		const clone = templateCard.content.cloneNode(true);
+		clone.querySelector("#cardImg").setAttribute("src", item.images[0]);
+		clone.querySelector("#cardName").textContent = item.title;
+		clone.querySelector("#cardPrice span").textContent = item.price;
+		clone.querySelector("#cardId").textContent = item.id;
+
+		fragment.appendChild(clone);
+	});
+
+	cards.appendChild(fragment);
 };
